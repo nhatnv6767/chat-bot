a72b728ccfe6a78f4b6359840b193f835a8275d1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 01:11:29 +0900

    #1 Init projects 6

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -3,7 +3,7 @@ import express from 'express';
 // config view engine for an express app
 let configViewEngine = (app) => {
     app.use(express.static("./src/public"))
-    app.set("view engine", ejs)
+    app.set("view engine", "ejs")
     app.set("views", "./src/views")
 }
 
