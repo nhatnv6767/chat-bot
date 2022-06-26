b8c124f6a2b051f7e4abf90c9772fcae7a99459b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 15:18:13 +0900

    #3 Getting name, profile information 4

---
+++ src/services/ChatBotService.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+import request from "request";
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+
+let callSendAPI = () => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v14.0/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
+let handleGetStarted = () => {
+
+}
+
+module.exports = {
+    handleGetStarted: handleGetStarted,
+}
