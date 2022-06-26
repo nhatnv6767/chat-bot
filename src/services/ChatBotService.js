054023044670aa7ebaaee03cd0989956ff6df375
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 15:43:11 +0900

    #3 Getting name, profile information 8

--- src/services/ChatBotService.js
+++ src/services/ChatBotService.js
@@ -26,11 +26,32 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
+let getUserName = async (sender_psid) => {
+    let username = ""
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "GET",
+    }, (err, res, body) => {
+        console.log(body)
+        if (!err) {
+            let response = JSON.parse(res)
+            console.log("CHECK USER SEND MESSAGE:", response)
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+    return username;
+}
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
             let response = { "text": "Chào mừng bạn XXX đến với nhà hàng của chúng tôi." }
             await callSendAPI(sender_psid, response)
+            await getUserName(sender_psid)
             resolve("done")
         } catch (e) {
             reject(e);
