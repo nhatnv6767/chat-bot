7d4e2ccc4379c5e2bbbead756138a7e41dc8afbd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 17:01:56 +0900

    #4 Messager template 3

--- src/services/ChatBotService.js
+++ src/services/ChatBotService.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 import request from "request";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+const IMAGE_GET_STARTED = "https://shorturl.at/jAQU2"
 
 let callSendAPI = (sender_psid, response) => {
     // Construct the message body
@@ -52,7 +53,13 @@ let handleGetStarted = (sender_psid) => {
         try {
             let username = await getUserName(sender_psid)
             let first_response = { "text": `Chào mừng bạn ${username} đến với nhà hàng của chúng tôi.` }
+            let second_response = sendGetStartedTemplate()
+
+            // send text message
             await callSendAPI(sender_psid, first_response)
+
+            // send generic template message
+            await callSendAPI(sender_psid, second_response)
 
             resolve("done")
         } catch (e) {
@@ -69,9 +76,9 @@ let sendGetStartedTemplate = () => {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Is this the right picture?",
-                    "subtitle": "Tap a button to answer.",
-                    "image_url": attachment_url,
+                    "title": "Xin chào bạn đến với nhà hàng của chúng tôi",
+                    "subtitle": "Dưới đây là các lựa chọn của nhà hàng.",
+                    "image_url": IMAGE_GET_STARTED,
                     "buttons": [
                         {
                             "type": "postback",
