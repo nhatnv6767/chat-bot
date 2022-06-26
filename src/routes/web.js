3cade36a68e95016bdfdc52f0b3db8640f77ca88
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 12:21:18 +0900

    #2 Get Started button 2

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,7 +5,8 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage)
-
+    
+    router.post("/setup-profile", homeController.setupProfile)
     // config webhook
     router.post("/webhook", homeController.postWebhook)
     router.get("/webhook", homeController.getWebhook)
