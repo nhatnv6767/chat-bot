1596c50308309dc3022f82b8834a612ef2547d1b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 17:36:52 +0900

    #4 Messager template 9

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -190,8 +190,52 @@ let setupProfile = async (req, res) => {
     return res.send("Setup user profile succeeds!")
 }
 
-let setupPersistentMenu = (req, res) => {
-    
+let setupPersistentMenu = async (req, res) => {
+// call facebook api profile
+    // Construct the message body
+    let request_body = {
+        "persistent_menu": [
+            {
+                "locale": "default",
+                "composer_input_disabled": false,
+                "call_to_actions": [
+                    {
+                        "type": "web_url",
+                        "title": "Channel Youtube",
+                        "url": "https://www.youtube.com/user/ANNnewsCH",
+                        "webview_height_ratio": "full"
+                    },
+                    {
+                        "type": "web_url",
+                        "title": "Facebook Page",
+                        "url": "https://www.facebook.com/Nh%C3%A0-h%C3%A0ng-None-101038356003024/",
+                        "webview_height_ratio": "full"
+
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Khởi động lại bot",
+                        "payload": "RESTART_BOT"
+                    }
+                ]
+            }
+        ]
+    }
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/v14.0/me/custom_user_settings?access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('Setup persistent menu succeeds!')
+        } else {
+            console.error("Unable to setup persistent menu:" + err);
+        }
+    });
+
+    return res.send("Setup persistent menu succeeds!")
 }
 
 module.exports = {
--- src/routes/web.js
+++ src/routes/web.js
@@ -6,7 +6,9 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage)
 
+    // setup get started button, whitelisted domain
     router.post("/setup-profile", homeController.setupProfile)
+    // setup persistent menu
     router.post("/setup-persistent-menu", homeController.setupPersistentMenu)
     // config webhook
     router.post("/webhook", homeController.postWebhook)
