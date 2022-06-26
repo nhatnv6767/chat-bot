00abd4df10cfde044e09b05720b017aa9600bffd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 11:46:25 +0900

    #1 Init projects 12 config heroku 9

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -71,13 +71,20 @@ let getWebhook = (req, res) => {
 function handleMessage(sender_psid, received_message) {
     let response;
 
-    // Check if the message contains text
+    // Checks if the message contains text
     if (received_message.text) {
 
-        // Create the payload for a basic text message
+        // Creates the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
         }
+
+    } else if (received_message.attachments) {
+
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+
     }
 
     // Sends the response message
