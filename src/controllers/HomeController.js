95ba571f73119bce6b08531aff33dad5d6eb0190
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-26 11:36:22 +0900

    #1 Init projects 12 config heroku 6

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -69,7 +69,19 @@ let getWebhook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Check if the message contains text
+    if (received_message.text) {
+
+        // Create the payload for a basic text message
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+        }
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
