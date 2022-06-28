8faea073640fb597baef67ca4e8106cc5b2360ed
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-28 21:24:27 +0900

    #8 Facebook Webview Reserve table 33 2

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -10,7 +10,7 @@
 window.extAsyncInit = function() {
     // the Messenger Extensions JS SDK is done loading
 
-    MessengerExtensions.getContext("101038356003024",
+    MessengerExtensions.getContext("449310003232049",
         function success(thread_context){
             // success
             //set psid to input
