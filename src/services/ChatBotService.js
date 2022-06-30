7e71cb4c05fb644ed82893d1c000e698f0b64d8a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-30 22:31:40 +0900

    #10 Quick replies and send gif img 5

--- src/services/ChatBotService.js
+++ src/services/ChatBotService.js
@@ -31,30 +31,38 @@ const IMAGE_DETAIL_ROOMS = "https://bit.ly/3nlgCUc"
 
 const IMAGE_GIF_WELCOME = "https://bit.ly/3nzlUex"
 
-let callSendAPI = async (sender_psid, response) => {
-    // Construct the message body
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    }
+let callSendAPI =  (sender_psid, response) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            // Construct the message body
+            let request_body = {
+                "recipient": {
+                    "id": sender_psid
+                },
+                "message": response
+            }
 
-    await sendMarkReadMessage(sender_psid)
-    await sendTypingOn(sender_psid)
-    // Send the HTTP request to the Messenger Platform
-    request({
-        "uri": "https://graph.facebook.com/v14.0/me/messages",
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
+            await sendMarkReadMessage(sender_psid)
+            await sendTypingOn(sender_psid)
+            // Send the HTTP request to the Messenger Platform
+            request({
+                "uri": "https://graph.facebook.com/v14.0/me/messages",
+                "qs": { "access_token": PAGE_ACCESS_TOKEN },
+                "method": "POST",
+                "json": request_body
+            }, (err, res, body) => {
+                if (!err) {
+                    console.log('message sent!')
+                } else {
+                    console.error("Unable to send message:" + err);
+                }
+            });
+
+        }catch (e) {
+            reject(e);
         }
-    });
+    })
+
 }
 
 let sendTypingOn = (sender_psid) => {
